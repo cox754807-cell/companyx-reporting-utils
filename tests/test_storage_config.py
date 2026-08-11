f905408ce06d202b0a1e2f6f70b115f432d20fcb
cox754807-cell <cox754807@gmail.com>
2026-08-11 22:30:13 +0100

    Delete tests directory

--- tests/test_storage_config.py
+++
@@ -1,4 +0,0 @@
-def test_expected_training_names():
-    assert "companyx-training-finance".startswith("companyx")
-    assert "companyxtraining".startswith("companyx")
-    assert "companyx-training-exports".startswith("companyx")
