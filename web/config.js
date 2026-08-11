298b4e17674ff9d1c52710d1ffe4a5fa809adc69
cox754807-cell <cox754807@gmail.com>
2026-08-11 22:30:22 +0100

    Delete web directory

--- web/config.js
+++
@@ -1,6 +0,0 @@
-window.COMPANYX_CONFIG = {
-  companyWebsite: "https://companyx.example",
-  analyticsExportRoot: "https://storage.googleapis.com/companyx-training-exports/quarterly/",
-  publicDataset: "payments_summary_2026.csv",
-  projectCode: "AURORA"
-};
