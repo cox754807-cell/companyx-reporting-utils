443b49c3cbd86e71667a54875bb2b245b967ea6b
cox754807-cell <cox754807@gmail.com>
2026-08-11 21:29:10 +0100

    Update config.js

--- web/config.js
+++ web/config.js
@@ -1,4 +1,5 @@
 window.COMPANYX_CONFIG = {
+  companyWebsite: "https://companyx.example",
   analyticsExportRoot: "https://storage.googleapis.com/companyx-training-exports/quarterly/",
   publicDataset: "payments_summary_2026.csv",
   projectCode: "AURORA"
